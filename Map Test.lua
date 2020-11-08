af8ec952bfc303fb545dd73b70264b6b36218498
nonfunctable <kernc4706@gmail.com>
2020-11-08 00:15:48 -0500

    Create Map Test.lua

---
+++ Map Test.lua
@@ -0,0 +1,5 @@
+game.ReplicatedStorage.Maps["0"]:Destroy()
+game.ReplicatedStorage.Maps["1"]:Destroy()
+wait(0.5)
+game:GetObjects("rbxassetid://5930996068")[1].Parent=game.ReplicatedStorage.Maps
+game:GetObjects("rbxassetid://5930978929")[1].Parent=game.ReplicatedStorage.Maps
