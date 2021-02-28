1ef60808ab55a97a33f4dbf5e284f92ac8e6e54f
nonfunctable <kernc4706@gmail.com>
2021-02-28 11:52:13 -0500

    Removed Old Scripts
    Removed Old Scripts

--- scythe.lua
+++
@@ -1,4 +0,0 @@
-game:GetService("Players")[game.Players.LocalPlayer.name].Backpack.Pickaxe.Handle.MeshId = "rbxassetid://28140935"
-game:GetService("Players")[game.Players.LocalPlayer.name].Backpack.Pickaxe.Handle.TextureID = "rbxassetid://27739981"
-game:GetService("Players")[game.Players.LocalPlayer.name].Backpack.Pickaxe.Handle.Orientation = Vector3.new(0, -70, 0)
-game:GetService("Players")[game.Players.LocalPlayer.name].Backpack.Pickaxe.Name = OG Scythe
